d044cd939052847fd2ee26031213738477be4423
tbow5257 <133tjb@gmail.com>
2018-02-25 11:55:06 -0800

    feat: Implement React-Router-Dom and dir restructre

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './components/index.css';
+import App from './components/App';
+import NotFound from './components/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const repo = `/${window.location.pathname.split('/')[1]}`;
+const Root = () => {
+    return (
+        <Router basename={repo}>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </Router>
+    )
+};
+
+render(<Root/>, document.querySelector('#root'));
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
