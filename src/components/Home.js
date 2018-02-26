3610174355d691f1c65a1419ba81ca36280cbf5f
tbow5257 <133tjb@gmail.com>
2018-02-25 20:20:04 -0800

    fix: different tooltip plugin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import ReactTooltip from 'react-tooltip';
 import github from '../assets/github.png'
 import twitter from '../assets/twitter.png'
 import icon from '../assets/noun.png'
@@ -34,7 +35,7 @@ const Contact = styled.div`
   img {
     height: 40px;
     padding-top: 5px;
-    margin: 0 15px 0 15px;
+    margin: 0 25px 0 25px;
   }
 `;
 
@@ -45,8 +46,9 @@ class Home extends Component {
             <Page>
                 <Logo> <img src={icon} alt='Dankbytes Icon'></img>Dankbytes</Logo>
                 <HelloText>Thomas Bowen's portfolio page</HelloText>
-                <Contact>   <img src={email} alt='Email'></img>
-                            <img src={github} alt='Github'></img>
+                <Contact>   <img src={email} alt='Email' data-tip="hello at tjbowen.me"></img>
+                    <ReactTooltip delayHide={1000}  />
+                    <img src={github} alt='Github'></img>
                             <img src={twitter} alt='Twitter'></img></Contact>
             </Page>
         );
