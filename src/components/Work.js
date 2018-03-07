1ca8a71da45249a10b4db07fd7cc035ba353fd92
tbow5257 <133tjb@gmail.com>
2018-03-06 18:31:33 -0800

    feat: Nav button and title

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,19 +1,46 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import {Button, Icon} from 'semantic-ui-react'
+
 
 const Page = styled.div`
-  display: flex;
+    display: flex;
+    
+    flex-direction: column;
+    
+    justify-content: space-between;
+    width: 100%;
+    margin: 10px;
+`;
 
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  margin: 10px;
+const Navigation = styled.div`
+    align-items: left;
+    justify-items: left;
+    
+    i {
+      margin: 4px 8px !important;
+      font-size: 1.7em !important;
+      }
 `;
 
 const Hi = styled.div`
-align-items: center;
-justify-items: center;
+    text-align: center;
+    font-size: 2.5em;
+`;
+
+const GridButtons = styled.div`
+    align-items: center;
+    justify-items: center;
+`;
+
+const Grid = styled.div`
+    align-items: center;
+    justify-items: center;
+`;
+
+const Resume = styled.div`
+    align-items: center;
+    justify-items: center;
 `;
 
 
@@ -21,13 +48,25 @@ export default class Work extends Component {
     render() {
         return (
             <Page>
+                <Navigation>
+                    <Button >
+                        <Icon name="arrow left" />
+                        <Icon name="home" />
+                    </Button>
+                </Navigation>
                 <Hi>
-                    <p>Hi</p>
-                    <p>Hi</p>
-                    <p>Hi</p>
-                    <p>Hi</p>
-                    <p>Hi</p>
+                    Work
                 </Hi>
+                <GridButtons>
+
+                </GridButtons>
+                <Grid>
+                </Grid>
+                <Resume>
+
+                </Resume>
+
+
             </Page>
             );
     }
